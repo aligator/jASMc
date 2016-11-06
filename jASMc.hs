f515d9cedbe311a4db253053581f9b0f7b80379d
aligator <aligator@suncraft-server.de>
2016-11-06 20:01:37 +0100

    erweitert

--- jASMc.hs
+++ jASMc.hs
@@ -1,31 +1,81 @@
 import System.IO
+import qualified Data.List.Split as Split
+import qualified Data.Char as Char
+import Data.Bits
+import Data.Word
 
+data Register = Register {regId :: Integer}  deriving (Show,Eq) 
+data Command = Command {cmdId :: Integer} deriving (Show,Eq) 
 
-data RegisterType = RegisterType {regId :: Integer}  deriving (Show,Eq) 
+getRegister :: String -> Register
+getRegister "a" = Register 0
+getRegister "b" = Register 1
+getRegister "c" = Register 2
+getRegister "d" = Register 3
+getRegister "e" = Register 4
+getRegister "f" = Register 5
+getRegister "g" = Register 6
+getRegister "h" = Register 7
+getRegister "w1" = Register 8
+getRegister "w2" = Register 9
+getRegister "w3" = Register 10
+getRegister "w4" = Register 11
+getRegister "ax" = Register 12
+getRegister "bx" = Register 13
+getRegister "rx" = Register 14
+getRegister s = Register (-1)
 
-getRegister :: String -> RegisterType
-getRegister "a" = RegisterType 0
-getRegister "b" = RegisterType 1
-getRegister "c" = RegisterType 2
-getRegister "d" = RegisterType 3
-getRegister "e" = RegisterType 4
-getRegister "f" = RegisterType 5
-getRegister "g" = RegisterType 6
-getRegister "h" = RegisterType 7
-getRegister "w1" = RegisterType 8
-getRegister "w2" = RegisterType 9
-getRegister "w3" = RegisterType 10
-getRegister "w4" = RegisterType 11
-getRegister "ax" = RegisterType 12
-getRegister "bx" = RegisterType 13
-getRegister "rx" = RegisterType 14
+getCommand :: String -> Command
+getCommand "Ende" = Command 0
+getCommand "I++" = Command 2
+getCommand "Print" = Command 8
+getCommand s = Command (-1)
 
-getRegId :: RegisterType -> Integer
+isInteger :: String -> Bool
+isInteger ""  = False
+isInteger "." = False
+isInteger xs  =
+  case dropWhile Char.isDigit xs of
+    ""       -> True
+    _        -> False
+
+getRegId :: Register -> Integer
 getRegId reg = regId reg
 
 getRegIdByStr :: String -> Integer
 getRegIdByStr reg = getRegId (getRegister reg)
 
+getCmdId :: Command -> Integer
+getCmdId cmd = cmdId cmd
+
+getCmdIdByStr :: String -> Integer
+getCmdIdByStr cmd = getCmdId (getCommand cmd)
+
+splitOnBlanc :: String -> [[Char]]
+splitOnBlanc str = Split.splitOn " " str
+
+extractCmdfromSplitted :: [[Char]] -> Integer
+extractCmdfromSplitted splitted = getCmdIdByStr (splitted !! 0)
+
+extractCmd :: String -> Integer
+extractCmd s = extractCmdfromSplitted (splitOnBlanc s)
+
+
+checkAsmIsInteger :: String -> Bool
+checkAsmIsInteger (x:xs) = x == '#' && isInteger xs
+
+checkAsmIsPointer :: String -> Bool
+checkAsmIsPointer = isInteger
+
+checkAsmIsRegister :: String -> Bool
+checkAsmIsRegister s = (getRegIdByStr s) >= 0
+
+asmAddCmd :: Word8 -> Word32
+asmAddCmd cmd = shiftL (Word32 cmd) 18
+
+-- extractData :: String -> Integer
+-- extractData s = extractCmdfromSplitted (splitOnBlanc s)
 
 main = do
-    putStrLn (show(regId (getRegister("a"))))
+    --putStrLn (show(extractCmd "asf sdf asf sdf"))
+    putStrLn (show(getCmdIdByStr "I++"))
